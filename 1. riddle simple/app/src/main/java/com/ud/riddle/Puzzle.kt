3982b83d4b31f3dd9c063b4096c7b0d5362c91e0
David7-7-7 <jefersonangel2017@gmail.com>
2026-03-07 19:41:35 -0500

    Añadimos colores a los botones y mejoramos la estructura

--- 1. riddle simple/app/src/main/java/com/ud/riddle/Puzzle.kt
+++ 1. riddle simple/app/src/main/java/com/ud/riddle/Puzzle.kt
@@ -1,7 +1,6 @@
 package com.ud.riddle
 
-import android.util.Log
-import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,25 +18,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun Puzzle(){
-
-
     Column(modifier = Modifier.fillMaxSize().padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(Modifier.height(40.dp))
         Text("Slide Puzzle",
             fontSize = 38.sp)
-
         TableroNumerico()
     }
 }
@@ -52,6 +50,7 @@ fun TableroNumerico() {
     var seleccion by remember { mutableStateOf<Int?>(null) } // Digamos cuando elegimo el 5 hace como una pregunta de "Ya elegiste el primer numero?"
     var moves by remember { mutableStateOf(0) }
     var Gane by remember { mutableStateOf(false) }
+    val movimientosMinimos = calcularMovimientosMinimos(numeros)
 
     fun Inicializamos(){
         numeros.clear()
@@ -61,7 +60,7 @@ fun TableroNumerico() {
         Gane = false
         seleccion = null
     }
-   LaunchedEffect(Unit) { //Ejecutamos el juego automaticamente
+    LaunchedEffect(Unit) { //Ejecutamos el juego automaticamente
         Inicializamos()
     }
 
@@ -75,23 +74,28 @@ fun TableroNumerico() {
         }
         Spacer(Modifier.height(20.dp))
         Text("Moves: $moves")
+        Text("Minimum moves: $movimientosMinimos")
         Spacer(Modifier.height(40.dp))
         if(solucion == numeros){
             Gane = true
-            Toast.makeText(context, "Ganaste 🎉", Toast.LENGTH_LONG).show()
         }
         Spacer(Modifier.height(40.dp))
         if (Gane) {
-            Inicializamos()
+            Ganaste(moves,onReset = {Inicializamos()})
         }
 
         Column() {
             // Creamos las 3 filas manualmente
-                for (i in 0 until 3) {
+            for (i in 0 until 3) {
                 Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
                     for (j in 0 until 3) {
                         val position = i * 3 + j //Posicion de cada numero
                         val valorNum = numeros[position] // El valor del numero seleccionado
+
+                        val borde = when {
+                            seleccion == position -> BorderStroke(3.dp, Color.Red) // primer botón
+                            else -> BorderStroke(1.dp, Color.Gray)
+                        }
 
                         // Cada celda es un botón
                         Button(
@@ -121,6 +125,7 @@ fun TableroNumerico() {
                                     seleccion = null
                                 }
                             },
+                            border=borde,
                             modifier = Modifier
                                 .size(80.dp)
                                 .padding(bottom = 8.dp)
@@ -134,8 +139,6 @@ fun TableroNumerico() {
     }
 }
 
-
-
 @Composable
 fun StyleButton(text: String, onClick: () -> Unit) {
     Button(
@@ -148,3 +151,51 @@ fun StyleButton(text: String, onClick: () -> Unit) {
     }
 }
 
+@Composable
+fun calcularMovimientosMinimos(tablero: List<String>): Int {
+    var distancia = 0
+
+    for (i in tablero.indices) {
+        val valor = tablero[i] //Tomamos el numero de la posicion
+
+        if (valor != "0") { // ignoramos el espacio vacío orque no es un aficha
+            val posnumobj = valor.toInt() - 1 // Le restamos 1 porque el incidice empieza en 0
+
+            val filaActual = i / 3
+            val colActual = i % 3
+
+            val filaObjetivo = posnumobj / 3
+            val colObjetivo = posnumobj % 3
+
+            distancia += kotlin.math.abs(filaActual - filaObjetivo) +
+                    kotlin.math.abs(colActual - colObjetivo)
+        }
+    }
+    return distancia
+}
+
+@Composable
+fun Ganaste(moves: Int, onReset: () -> Unit){
+    AlertDialog(
+        onDismissRequest = {},
+        title = {
+            Text(
+                text = "🎉 ¡GANASTE! 🎉",
+                fontSize = 30.sp
+            )
+        },
+        text = {
+            Text(
+                "Terminaste el puzzle en $moves movimientos.\n\n¡Buen trabajo!",
+                fontSize = 18.sp
+            )
+        },
+        confirmButton = {
+            Button(onClick = {
+                onReset()
+            }) {
+                Text("Jugar otra vez")
+            }
+        }
+    )
+}
